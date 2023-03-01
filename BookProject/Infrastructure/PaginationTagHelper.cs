c49a4472c9743d01d45d33a58cd1704505f78da0
Michael Chase <mchase62@byu.edu>
2023-03-01 10:55:15 -0700

    First commit

---
+++ BookProject/Infrastructure/PaginationTagHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookProject.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace BookProject.Models.Infrastructure
+{
+    [HtmlTargetElement("div", Attributes = "page-bop")]
+    public class PaginationTagHelper : TagHelper
+    {
+        //Dynamically create the page links for us
+        private IUrlHelperFactory uhf;
+
+        public PaginationTagHelper (IUrlHelperFactory temp)
+        {
+            uhf = temp;
+        }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext vc { get; set; }
+
+        // Different than the View Context
+        public PageInfo PageBop { get; set; } // connected to the index.cshtml page-bop div
+        public string PageAction { get; set; }
+
+        public override void Process(TagHelperContext thc, TagHelperOutput tho)
+        {
+            IUrlHelper uh = uhf.GetUrlHelper(vc);
+
+            TagBuilder final = new TagBuilder("div");
+
+            for (int i = 1; i <= PageBop.TotalPages; i++)
+            {
+                TagBuilder tb = new TagBuilder("a");
+
+                tb.Attributes["href"] = uh.Action(PageAction, new { pageNum = i });
+                tb.InnerHtml.Append(i.ToString());
+
+                final.InnerHtml.AppendHtml(tb);
+            }
+            tho.Content.AppendHtml(final.InnerHtml);
+        }
+    }
+}
